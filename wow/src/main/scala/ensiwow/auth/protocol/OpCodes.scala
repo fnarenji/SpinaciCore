201ae76d378aca5848f06a297418d33cbe2e6270
sknz <florandara@gmail.com>
2017-02-10 18:27:33 +0100

    [auth] ClientLogonChallenge, constantE, opcodes, AuthPacketTest, ClientLogonChallengeTest
    * constantE provides support for using instances of encodable classes as
    constants
    * OpCodes represents opcode enumeration, encodable
    * IP as vector of 4 int instead of uint32
    * AuthPacketTest provides generic way to compare packet instance with
    decoded hex packet
    * ClientLogonChallengeTest is test for client logon challenge

---
+++ wow/src/main/scala/ensiwow/auth/protocol/OpCodes.scala
@@ -0,0 +1,25 @@
+package ensiwow.auth.protocol
+
+import scodec._
+import scodec.codecs._
+
+object OpCodes {
+  final case class OpCode(opCode : Int)
+
+  val LogonChallenge = OpCode(0x0)
+  val LogonProof = OpCode(0x1)
+  val ReconnectChallenge = OpCode(0x2)
+  val ReconnectProof = OpCode(0x3)
+  val RealmList = OpCode(0x10)
+
+  /**
+    * Those below are not supported
+    */
+  val TransferInitiate = OpCode(0x30)
+  val TransferData = OpCode(0x31)
+  val TransferAccept = OpCode(0x32)
+  val TransferResume = OpCode(0x33)
+  val TransferCancel = OpCode(0x34)
+
+  implicit val codec: Codec[OpCode] = { "opCode" | uint8L }.as[OpCode]
+}
