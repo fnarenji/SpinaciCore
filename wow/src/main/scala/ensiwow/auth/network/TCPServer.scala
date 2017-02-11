66d522210f8f89d591953f2d2aee7f48d190e2c1
Yann Colina <yann.colina@grenoble-inp.org>
2017-02-11 18:46:39 +0100

    [tcp server] Adding a handler class
    Now, all the data received by the server pass through a handler object.
    For now, it stores the incoming ByteString in a local buffer.

--- wow/src/main/scala/ensiwow/auth/network/TCPServer.scala
+++ wow/src/main/scala/ensiwow/auth/network/TCPServer.scala
@@ -2,45 +2,82 @@ package ensiwow.auth.network
 
 import java.net.InetSocketAddress
 
-import akka.actor.{Actor, Terminated}
+import akka.actor.{Actor, ActorRef, Props, Terminated}
 import akka.event.Logging
 import akka.io.{IO, Tcp}
 import akka.io.Tcp._
+import akka.util.{ByteString, Timeout}
+import akka.pattern.ask
+import akka.testkit.TestActorRef
 
+import scala.concurrent.Future
+import scala.concurrent.duration._
+import scala.util.{Failure, Success}
+import scala.concurrent.ExecutionContext.Implicits.global
 
 /**
   * Created by yanncolina on 08/02/17.
   * This class defines the behaviour of the main TCP server.
   * @constructor send a Bind command to the TCP manager
   */
+
+case class GetBufferSize()
+case class GetData()
+case class GetAddress()
+
 class TCPServer extends Actor {
     import context.system
 
+    implicit val timeout : Timeout = 2 seconds
+
     val log = Logging.getLogger(context.system, this)
+    var handlerRef : ActorRef = null
     var address = ""
 
-    log.info("Binding server with socket")
+    log.debug("[TCPSERVER] Binding server with socket")
     IO(Tcp) ! Bind(self, new InetSocketAddress("localhost", 5555))
 
-    override def postStop(): Unit = log.info("TCPServer is being stopped")
+    override def postStop(): Unit = log.info("[TCPSERVER] Stopped")
 
     def receive = {
 
-        case 42 => sender() ! 42
+        case 42         => sender() ! 42
+        case GetAddress => sender() ! address
+        case GetData    =>
+            log.debug("[TCPSERVER] Retrieving data stored in the handler's buffer")
+            try {
+                val future: Future[ByteString] = (handlerRef ? "getData").mapTo[ByteString]
+                log.debug(s"[TCPSERVER] Retrieved: $future")
+                if (future.isCompleted) {
+                    val Success(data: ByteString) = future.value.get
+                    sender() ! data
+                }
+            } catch {
+                case EmptyBufferException(msg) => println(s"$msg")
+            }
 
-        case "address?" => sender() ! address
+        case GetBufferSize =>
+            log.debug("[TCPSERVER] Getting buffer size")
+            val future: Future[Int] = (handlerRef ? GetBufferSize).mapTo[Int]
+            future onComplete {
+                case Success(size: Int) =>
+                    log.debug(s"[TCPSERVER] Buffer size received: $size")
+                    sender() ! size
+                case Failure(t) =>
+                    log.debug("[TCPTSERVER] An error has occured: " + t.getMessage)
+            }
 
         case b @ Bound(localAddress) =>
-            log.info("TCP port opened")
-            log.info("IP address: " + localAddress.getHostString())
-            log.info("Port opened: " + localAddress.getPort())
+            log.debug("[TCPSERVER] TCP port opened at: " + localAddress.getPort())
             address = localAddress.getHostString()
 
         case c @ Connected(remote, local) =>
-            log.info("Remote connection set")
+            log.debug("[TCPSERVER] Remote connection set from: " + local + " to: " + remote)
+            val connection = sender()
+            handlerRef = context.actorOf(Props(classOf[BasicHandler], connection))
+            connection ! Register(handlerRef)
 
         case CommandFailed(_: Bind) => context stop self
-
     }
 }
 
