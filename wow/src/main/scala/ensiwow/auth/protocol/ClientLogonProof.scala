b44b96d9c0e8459f199f8b52a9df8ef838a3f8f7
sknz <florandara@gmail.com>
2017-02-13 00:17:57 +0100

    [auth] some code styling in ClientLogonProof

--- wow/src/main/scala/ensiwow/auth/protocol/ClientLogonProof.scala
+++ wow/src/main/scala/ensiwow/auth/protocol/ClientLogonProof.scala
@@ -1,34 +1,29 @@
 package ensiwow.auth.protocol
 
+import ensiwow.auth.protocol.codecs._
 import scodec._
 import scodec.codecs._
-import ensiwow.auth.protocol.AuthResults.AuthResult
-import ensiwow.auth.protocol.codecs._
-import ensiwow.auth.protocol.{AuthResults, OpCodes}
 
 /**
   * Created by sknz on 2/7/17.
   */
 case class ClientLogonProof(opCode: Int,
-                                A: BigInt,
-                                M1: BigInt,
-                                crc_hash: BigInt,
+                            A: BigInt,
+                            M1: BigInt,
+                            crc_hash: BigInt,
                             number_of_keys: Int,
-                            securityFlags: Int
-                           ) {
-  }
+                            securityFlags: Int)
 
 object ClientLogonProof {
-
   final val ALength = 32
   final val M1Length = 20
   final val CRCLength = 20
 
   implicit val codec: Codec[ClientLogonProof] = {
-      ("opCode" | uint8L) ::
-      ("A" | fixedUBigIntL(32)) ::
-      ("M1" | fixedUBigIntL(20)) ::
-      ("crc_hash" | fixedUBigIntL(20)) ::
+    ("opCode" | uint8L) ::
+      ("A" | fixedUBigIntL(ALength)) ::
+      ("M1" | fixedUBigIntL(M1Length)) ::
+      ("crc_hash" | fixedUBigIntL(CRCLength)) ::
       ("number_of_keys" | uint8L) ::
       ("securityFlags" | uint8L)
   }.as[ClientLogonProof]
