8a15c0b6a9db18963a2af51c559376b7386055d8
sknz <florandara@gmail.com>
2017-02-12 16:33:32 +0100

    [auth] add bignum codec test

---
+++ wow/src/test/scala/ensiwow/auth/protocol/codecs/FixedUnsignedBigIntCodecTest.scala
@@ -0,0 +1,88 @@
+package ensiwow.auth.protocol.codecs
+
+import org.scalatest.{FlatSpec, Matchers}
+import scodec.Attempt.{Failure, Successful}
+import scodec.DecodeResult
+import scodec.bits._
+
+/**
+  * Created by sknz on 2/12/17.
+  */
+class FixedUnsignedBigIntCodecTest extends FlatSpec with Matchers {
+  "A FixedUnsignedBigIntCodec" must "fail on negative number" in {
+    val codec = fixedUBigIntL(32)
+    val value = BigInt("-983129873182379123791298731289319723712798318927379123725")
+    assertThrows[IllegalArgumentException] {
+      codec.encode(value)
+    }
+  }
+
+  it must "fail on over-capacity number" in {
+    val codec = fixedUBigIntL(4)
+    val value = BigInt(Long.MaxValue)
+
+    assertThrows[IllegalArgumentException] {
+      codec.encode(value)
+    }
+  }
+
+  it must "succeed on max number" in {
+    val codec = fixedUBigIntL(4)
+    val value = BigInt(0xFFFFFFFFL)
+
+    val attempt = codec.encode(value)
+
+    attempt match {
+      case Failure(err) => fail(err.toString())
+      case Successful(bits) =>
+        bits shouldEqual hex"FFFFFFFF".bits
+
+        val decode = codec.decode(bits)
+        decode match {
+          case Successful(DecodeResult(decoded, BitVector.empty)) => decoded shouldEqual value
+          case Successful(DecodeResult(_, remainder)) => fail(s"non empty remainder: $remainder")
+          case Failure(err) => fail(err.toString())
+        }
+    }
+  }
+
+  it must "succeed on in range number" in {
+    val codec = fixedUBigIntL(4)
+    val value = BigInt(0xFFFFL)
+
+    val attempt = codec.encode(value)
+
+    attempt match {
+      case Failure(err) => fail(err.toString())
+      case Successful(bits) =>
+        bits shouldEqual hex"FFFF0000".bits
+
+        val decode = codec.decode(bits)
+        decode match {
+          case Successful(DecodeResult(decoded, BitVector.empty)) => decoded shouldEqual value
+          case Successful(DecodeResult(_, remainder)) => fail(s"non empty remainder: $remainder")
+          case Failure(err) => fail(err.toString())
+        }
+    }
+  }
+
+  it must "succeed on zero" in {
+    val codec = fixedUBigIntL(4)
+    val value = BigInt(0)
+
+    val attempt = codec.encode(value)
+
+    attempt match {
+      case Failure(err) => fail(err.toString())
+      case Successful(bits) =>
+        bits shouldEqual hex"00000000".bits
+
+        val decode = codec.decode(bits)
+        decode match {
+          case Successful(DecodeResult(decoded, BitVector.empty)) => decoded shouldEqual value
+          case Successful(DecodeResult(_, remainder)) => fail(s"non empty remainder: $remainder")
+          case Failure(err) => fail(err.toString())
+        }
+    }
+  }
+}
