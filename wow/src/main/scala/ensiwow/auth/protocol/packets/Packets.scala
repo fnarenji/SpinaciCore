6bcf7286b7ed2ee199444f770603696f93c9353c
sknz <florandara@gmail.com>
2017-02-13 15:36:39 +0100

    [auth] add client/server packet traits

---
+++ wow/src/main/scala/ensiwow/auth/protocol/packets/Packets.scala
@@ -0,0 +1,11 @@
+package ensiwow.auth.protocol.packets
+
+/**
+  * Simple trait signaling a client packet
+  */
+trait ClientPacket
+
+/**
+  * Simple trait signaling a server packet
+  */
+trait ServerPacket
