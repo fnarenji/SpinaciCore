7b45937852aeebec9e3783804cc77cb315cf1924
sknz <florandara@gmail.com>
2017-02-13 15:47:54 +0100

    [auth] code styl in AuthSessionErrors

--- wow/src/main/scala/ensiwow/auth/session/AuthSessionErrors.scala
+++ wow/src/main/scala/ensiwow/auth/session/AuthSessionErrors.scala
@@ -12,7 +12,8 @@ case class MalformedPacketHeaderException(err: Err) extends IOException(s"Malfor
 
 case class MalformedPacketException(err: Err) extends IOException(s"Malformed packet: $err")
 
-case class PacketPartialReadException(remainder: BitVector) extends IOException(s"Invalid packet partial read: $remainder")
+case class PacketPartialReadException(remainder: BitVector) extends IOException(s"Invalid packet partial read: " +
+  s"$remainder")
 
 case class PacketSerializationException(err: Err) extends IOException(s"Packet couldn't be written: $err")
 
