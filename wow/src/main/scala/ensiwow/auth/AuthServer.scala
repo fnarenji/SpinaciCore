094ce38c32e59e76231738d933bf6cd5c457c062
sknz <florandara@gmail.com>
2017-02-13 23:24:38 +0100

    [auth] impl. dummy response for ServerLogonProof
    Always responds with auth failure.

--- wow/src/main/scala/ensiwow/auth/AuthServer.scala
+++ wow/src/main/scala/ensiwow/auth/AuthServer.scala
@@ -1,7 +1,7 @@
 package ensiwow.auth
 
 import akka.actor.{Actor, ActorLogging, Props}
-import ensiwow.auth.handlers.LogonChallengeHandler
+import ensiwow.auth.handlers.{LogonChallengeHandler, LogonProofHandler}
 import ensiwow.auth.network.TCPServer
 import ensiwow.auth.protocol.VersionInfo
 
@@ -16,6 +16,7 @@ class AuthServer extends Actor with ActorLogging {
 
   // TODO: handlers should be put in a pool so that they scale according to the load
   context.actorOf(LogonChallengeHandler.props, LogonChallengeHandler.PreferredName)
+  context.actorOf(LogonProofHandler.props, LogonProofHandler.PreferredName)
   context.actorOf(TCPServer.props, TCPServer.PreferredName)
 
   override def receive: Receive = PartialFunction.empty
@@ -27,4 +28,5 @@ object AuthServer {
   val PreferredName = "AuthServer"
   val ActorPath = s"${Application.actorPath}/$PreferredName"
   val LogonChallengeHandlerPath = s"$ActorPath/${LogonChallengeHandler.PreferredName}"
+  val LogonProofHandlerPath = s"$ActorPath/${LogonProofHandler.PreferredName}"
 }
