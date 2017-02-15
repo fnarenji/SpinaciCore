5b10e00e6d8291cb087cf31c6574884712633acd
sknz <florandara@gmail.com>
2017-02-15 10:17:10 +0100

    [auth] add some comments to AuthServer

--- wow/src/main/scala/ensiwow/auth/AuthServer.scala
+++ wow/src/main/scala/ensiwow/auth/AuthServer.scala
@@ -6,11 +6,15 @@ import ensiwow.auth.network.TCPServer
 import ensiwow.auth.protocol.VersionInfo
 
 /**
-  * Created by sknz on 2/13/17.
+  * AuthServer is the base actor for all services provided by the authentication server.
+  *
+  * This actor is unique (e.g. singleton) per ActorSystem.
+  * It holds ownership of the stateless packet handlers.
   */
 class AuthServer extends Actor with ActorLogging {
   log.info(s"startup, supporting version ${VersionInfo.SupportedVersionInfo}")
 
+  // TODO: handlers should be put in a pool so that they scale according to the load
   context.actorOf(LogonChallengeHandler.props, LogonChallengeHandler.PreferredName)
   context.actorOf(TCPServer.props, TCPServer.PreferredName)
 
