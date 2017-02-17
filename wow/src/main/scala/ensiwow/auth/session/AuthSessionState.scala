9f46bc9dbf1afd40f59d6dfed16b8a85dbb404ec
sknz <florandara@gmail.com>
2017-02-18 00:21:56 +0100

    [auth] implement reconnect logic for authsession fsm

--- wow/src/main/scala/ensiwow/auth/session/AuthSessionState.scala
+++ wow/src/main/scala/ensiwow/auth/session/AuthSessionState.scala
@@ -5,6 +5,8 @@ package ensiwow.auth.session
   */
 sealed trait AuthSessionState
 
+case object StateNoData extends AuthSessionState
+
 case object StateChallenge extends AuthSessionState
 
 case object StateProof extends AuthSessionState
@@ -12,3 +14,5 @@ case object StateProof extends AuthSessionState
 case object StateFailed extends AuthSessionState
 
 case object StateRealmlist extends AuthSessionState
+
+case object StateReconnectChallenge extends AuthSessionState
