3871e4a858b90bad678e70fa459746762f1f11bf
sknz <florandara@gmail.com>
2017-02-18 17:38:10 +0100

    [auth] realmlist fix build

--- wow/src/main/scala/ensiwow/auth/protocol/packets/ServerRealmlistPacket.scala
+++ wow/src/main/scala/ensiwow/auth/protocol/packets/ServerRealmlistPacket.scala
@@ -1,10 +1,11 @@
 package ensiwow.auth.protocol.packets
 
-import scala.language.postfixOps
 import ensiwow.auth.protocol.codecs._
 import ensiwow.auth.protocol.{OpCodes, VersionInfo}
 import scodec._
 import scodec.codecs._
+
+import scala.language.postfixOps
 
 /**
   * Created by yanncolina on 15/02/17.
@@ -22,7 +23,6 @@ case class RealmlistPacket(realmType: Int,
                            versionInfo: VersionInfo)
 
 case class ServerRealmlistPacket(packetSize: Int,
-                                 voidInt: Int,
                                  nbrRealms: Int,
                                  realms: Vector[RealmlistPacket],
                                  fixedSlot1: Int,
@@ -30,13 +30,12 @@ case class ServerRealmlistPacket(packetSize: Int,
 
 object RealmlistPacket {
   implicit val codec: Codec[RealmlistPacket] = {
-    def reversedFixedSizeCString(sizeInBytes: Long) = reverse(fixedCString(sizeInBytes))
     ("realmType" | uint8L) ::
       ("lock" | uint8L) ::
       ("flag" | uint8L) ::
-      ("name" | variableSizeBytes(uint8L, "name" | ascii))
-      ("ip" | reversedFixedSizeCString(4)) ::
-      ("populationLevel" | uint8L) ::
+      ("name" | reverse(cstring)) ::
+      ("ip" | reverse(cstring)) ::
+      ("populationLevel" | floatL) ::
       ("characterCount" | uint8L) ::
       ("timezone" | uint8L) ::
       ("id" | uint8L) ::
@@ -45,13 +44,14 @@ object RealmlistPacket {
 }
 
 object ServerRealmlistPacket {
- implicit val codec: Codec[ServerRealmlistPacket] = {
-   constantE(OpCodes.RealmList) ::
-     ("packetSize" | uint8L) ::
-     ("voidInt" | uint32L) ::
-     (("nbrRealms" | uint8L) >>:~ { nbrRealms => ("realms" | vectorOfN(provide(nbrRealms), Codec[RealmlistPacket])).hlist })
-     ("fixedSlot1" | uint8L) ::
-     ("fixedSlot1" | uint8L)
- }.as[ServerRealmlistPacket]
+  implicit val codec: Codec[ServerRealmlistPacket] = {
+    constantE(OpCodes.RealmList) ::
+      ("packetSize" | uint8L) ::
+      (("nbrRealms" | uint8L) >>:~ { nbrRealms =>
+        ("realms" | vectorOfN(provide(nbrRealms), Codec[RealmlistPacket])) ::
+          ("fixedSlot1" | uint8L) ::
+          ("fixedSlot2" | uint8L)
+      })
+  }.as[ServerRealmlistPacket]
 }
 
