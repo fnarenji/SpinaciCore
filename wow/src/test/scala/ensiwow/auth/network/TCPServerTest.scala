6ffae28e1b481e16ddbb6ce17a8d400b426818d9
Yann Colina <yann.colina@grenoble-inp.org>
2017-02-20 22:07:06 +0100

    [tcp test] A version working on my computer
    Need to be tested on Travis

--- wow/src/test/scala/ensiwow/auth/network/TCPServerTest.scala
+++ wow/src/test/scala/ensiwow/auth/network/TCPServerTest.scala
@@ -1,35 +1,26 @@
 package ensiwow.auth.network
 
 import org.scalatest.FlatSpec
+import akka.actor.{ActorRef, ActorSystem}
 import akka.testkit.TestActorRef
 import akka.pattern.ask
-
-import scala.util.{Failure, Success}
-import akka.actor.ActorSystem
 import akka.util.Timeout
 
+import scala.util.{Failure, Success}
 import scala.concurrent.duration._
-import scala.concurrent.Future
-
-import scala.concurrent.ExecutionContext.Implicits.global
+import scala.language.postfixOps
 
 /**
-  * Created by yanncolina on 09/02/17.
+  * Testing the TCP server
   */
-
-case class ServerNotBoundException(msg: String) extends Exception(msg)
 class TCPServerTest extends FlatSpec {
+  implicit val timeout = Timeout(2 second)
+  implicit val system = ActorSystem()
 
-    implicit val timeout : Timeout = 2 seconds
-    implicit val system = ActorSystem()
-
-    val serverRef = TestActorRef(new TCPServer)
-
-    "A server" should "be binded when created" in {
-        val future: Future[String] = (serverRef ? GetAddress).mapTo[String]
-        future onComplete {
-            case Success(result: String) => assert(result === "127.0.0.1")
-            case Failure(t)              => fail("Server not bound")
-        }
-    }
+  val serverRef: ActorRef = TestActorRef(new TCPServer)
+  "A server" should "be binded when created" in {
+    val future = (serverRef ? GetAddress).mapTo[String]
+    val Success(address: String) = future.value.get
+    assert(address === "127.0.0.1")
+  }
 }
