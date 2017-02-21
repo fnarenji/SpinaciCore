967674560b56e56760045c63738526f2395f43c5
Yann Colina <yann.colina@grenoble-inp.org>
2017-02-21 12:09:44 +0100

    [tcp test] Did some cleanup
    An InetSocketAddress is compared in the test and not only a InetAddress

--- wow/src/test/scala/ensiwow/auth/network/TCPServerTest.scala
+++ wow/src/test/scala/ensiwow/auth/network/TCPServerTest.scala
@@ -1,5 +1,7 @@
 package ensiwow.auth.network
 
+import java.net.InetSocketAddress
+
 import org.scalatest.FlatSpec
 import akka.actor.{ActorRef, ActorSystem}
 import akka.testkit.TestActorRef
@@ -7,7 +9,6 @@ import akka.pattern.ask
 import akka.util.Timeout
 
 import scala.concurrent.ExecutionContext.Implicits.global
-
 import scala.util.{Failure, Success}
 import scala.concurrent.duration._
 import scala.language.postfixOps
@@ -21,9 +22,9 @@ class TCPServerTest extends FlatSpec {
 
   val serverRef: ActorRef = TestActorRef(new TCPServer)
   "A server" must "be bound when created" in {
-    val future = (serverRef ? GetAddress).mapTo[String]
+    val future = (serverRef ? GetSocketAddress).mapTo[InetSocketAddress]
     future onComplete {
-      case Success(address) => assert(address === "127.0.0.1")
+      case Success(address) => assert(address.getHostString + ":" + address.getPort === "127.0.0.1:3724")
       case Failure(t)       => assert(false)
     }
   }
