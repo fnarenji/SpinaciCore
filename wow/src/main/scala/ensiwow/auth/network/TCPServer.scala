c04fd9c376240779adb544fb6276de74d52f3797
Yann Colina <yann.colina@grenoble-inp.org>
2017-02-21 12:09:44 +0100

    [tcp test] Did some cleanup
    An InetSocketAddress is compared in the test and not only a InetAddress

--- wow/src/main/scala/ensiwow/auth/network/TCPServer.scala
+++ wow/src/main/scala/ensiwow/auth/network/TCPServer.scala
@@ -5,12 +5,8 @@ import java.net.InetSocketAddress
 import akka.actor.{Actor, ActorLogging, Props}
 import akka.io.Tcp._
 import akka.io.{IO, Tcp}
-import akka.util.Timeout
 
-import scala.concurrent.duration._
-import scala.language.postfixOps
-
-case object GetAddress
+case object GetSocketAddress
 
 /**
   * This class defines the behaviour of the main TCP server.
@@ -21,21 +17,19 @@ class TCPServer extends Actor with ActorLogging {
   val bindAddress = "127.0.0.1"
   val bindPort = 3724
 
-  implicit val timeout: Timeout = 2 seconds
-
-  var address = ""
+  var address: InetSocketAddress = _
 
   log.debug("Binding server with socket")
   IO(Tcp)(context.system) ! Bind(self, new InetSocketAddress(bindAddress, bindPort))
 
-  override def postStop(): Unit = log.debug("Stopped")
+  override def postStop: Unit = log.debug("Stopped")
 
-  def receive = {
-    case GetAddress => sender() ! address
+  def receive: PartialFunction[Any, Unit] = {
+    case GetSocketAddress => sender() ! address
 
     case Bound(localAddress) =>
-      log.debug(s"TCP port opened at: ${localAddress.getPort}")
-      address = localAddress.getHostString
+      log.debug(s"TCP port opened at: ${localAddress.getHostString}:${localAddress.getPort}")
+      address = localAddress
 
     case Connected(remote, local) =>
       log.debug(s"Remote connection set from: $local to: $remote")
