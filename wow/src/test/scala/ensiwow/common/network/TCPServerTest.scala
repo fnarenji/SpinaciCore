341f79c3fe5c30082f629deb3760a9031a23acf3
sknz <florandara@gmail.com>
2017-02-26 20:41:02 +0100

    [network] finally fix tcp server test

--- wow/src/test/scala/ensiwow/common/network/TCPServerTest.scala
+++ wow/src/test/scala/ensiwow/common/network/TCPServerTest.scala
@@ -7,29 +7,25 @@ import akka.pattern.ask
 import akka.testkit.TestActorRef
 import akka.util.Timeout
 import ensiwow.auth.session.AuthSession
-import org.scalatest.FlatSpec
+import org.scalatest.{AsyncFlatSpec, Matchers}
 
-import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration._
 import scala.language.postfixOps
-import scala.util.{Failure, Success}
 
 /**
   * Testing the TCP server
   */
-class TCPServerTest extends FlatSpec {
+class TCPServerTest extends AsyncFlatSpec with Matchers {
   implicit val timeout = Timeout(2 second)
   implicit val system = ActorSystem()
 
-  val address = "127.0.0.1"
-  val port = 3724
+  val expectedHostname = "127.0.0.1"
+  val expectedPort = 3724
+  val expectedAddress = new InetSocketAddress(expectedHostname, expectedPort)
 
-  val serverRef: ActorRef = TestActorRef(TCPServer.props(AuthSession, address, port))
+  val serverRef: ActorRef = TestActorRef(TCPServer.props(AuthSession, expectedHostname, expectedPort))
   "A server" must "be bound when created" in {
     val future = (serverRef ? GetAddress).mapTo[InetSocketAddress]
-    future onComplete {
-      case Success(boundAddress) => assert(boundAddress.getHostString + ":" + boundAddress.getPort === "127.0.0.1:3724")
-      case Failure(t) => fail(t)
-    }
+    future map { _ shouldEqual expectedAddress }
   }
 }
