fdc2ddd9d9cd95259bb148447f2a496c7477c3ee
sknz <florandara@gmail.com>
2017-02-27 13:57:56 +0100

    [realm] empty realm server

--- wow/src/main/scala/ensiwow/Application.scala
+++ wow/src/main/scala/ensiwow/Application.scala
@@ -2,6 +2,7 @@ package ensiwow
 
 import akka.actor.ActorSystem
 import ensiwow.auth.AuthServer
+import ensiwow.realm.RealmServer
 
 /**
   * Created by sknz on 1/31/17.
@@ -11,6 +12,7 @@ object Application {
     val system = ActorSystem("EnsiWoW")
 
     system.actorOf(AuthServer.props, AuthServer.PreferredName)
+    system.actorOf(RealmServer.props, RealmServer.PreferredName)
   }
 
   val actorPath = "akka://EnsiWoW/user"
--- wow/src/main/scala/ensiwow/realm/RealmServer.scala
+++ wow/src/main/scala/ensiwow/realm/RealmServer.scala
@@ -3,8 +3,8 @@ package ensiwow.realm
 import akka.actor.{Actor, ActorLogging, Props}
 import ensiwow.Application
 import ensiwow.auth.protocol.VersionInfo
-import ensiwow.auth.session.AuthSession
 import ensiwow.common.network.TCPServer
+import ensiwow.realm.session.RealmSession
 
 /**
   * RealmServer is the base actor for all services provided by the realm server.
@@ -15,13 +15,13 @@ class RealmServer extends Actor with ActorLogging {
 
   log.info(s"startup, supporting version ${VersionInfo.SupportedVersionInfo}")
 
-  context.actorOf(TCPServer.props(AuthSession, address, port), TCPServer.PreferredName)
+  context.actorOf(TCPServer.props(RealmSession, address, port), TCPServer.PreferredName)
 
   override def receive: Receive = PartialFunction.empty
 }
 
 object RealmServer {
-  def props: Props = Props(new RealmServer)
+  def props: Props = Props(classOf[RealmServer])
 
   val PreferredName = "RealmServer"
   val ActorPath = s"${Application.actorPath}/$PreferredName"
