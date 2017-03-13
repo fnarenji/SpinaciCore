77ea509a12b75a6ce0151fd74c94f5b86884eef1
sknz <florandara@gmail.com>
2017-03-13 01:09:50 +0100

    [misc] cleanup comment in ReversedStringCodec

--- wow/src/main/scala/ensiwow/common/codecs/ReversedStringCodec.scala
+++ wow/src/main/scala/ensiwow/common/codecs/ReversedStringCodec.scala
@@ -1,11 +1,11 @@
 package ensiwow.common.codecs
 
-import scodec.Attempt.{Failure, Successful}
+import scodec.Attempt.Successful
 import scodec.bits.BitVector
 import scodec.{Attempt, Codec, DecodeResult, SizeBound}
 
 /**
-  * Created by sknz on 2/7/17.
+  * Codec reversing a String.
   */
 private[codecs] final class ReversedStringCodec(stringCodec: Codec[String]) extends Codec[String] {
   override def sizeBound: SizeBound = stringCodec.sizeBound
