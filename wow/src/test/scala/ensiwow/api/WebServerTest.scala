6ba089ae9af5a6a3749042065d81afb5391eb140
sknz <florandara@gmail.com>
2017-03-14 14:50:19 +0100

    [api] code style cleanups

--- wow/src/test/scala/ensiwow/api/WebServerTest.scala
+++ wow/src/test/scala/ensiwow/api/WebServerTest.scala
@@ -6,27 +6,27 @@ import akka.http.scaladsl.model.StatusCodes
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
 import spray.json.DefaultJsonProtocol._
 import spray.json.RootJsonFormat
-import ensiwow.auth.data.Account.User
+import ensiwow.auth.data.Account
 
 /**
   * Testing WebServer's responses to account's oriented requests.
   */
 class WebServerTest extends WordSpec with Matchers with ScalatestRouteTest {
-  implicit val userFormat: RootJsonFormat[User] = jsonFormat2(User)
+  implicit val userFormat: RootJsonFormat[Account] = jsonFormat2(Account.apply)
 
   "The service" should {
     "return a creation success code when an account is create" in {
-      Post("/user/create", User("myName", "myPass")) ~> WebServer.route ~> check {
+      Post("/account/create", Account("myName", "myPass")) ~> WebServer.route ~> check {
         status shouldEqual StatusCodes.Created
       }
     }
     "return a success code when an account is deleted" in {
-      Post("/user/delete", "myName") ~> WebServer.route ~> check {
+      Post("/account/delete", "myName") ~> WebServer.route ~> check {
         status shouldEqual StatusCodes.OK
       }
     }
     "return a success code when a password is reinitialized" in {
-      Put("/user/reinitialize", User("myName", "myPass")) ~> WebServer.route ~> check {
+      Put("/account/reinitialize", Account("myName", "myPass")) ~> WebServer.route ~> check {
         status shouldEqual StatusCodes.OK
       }
     }
