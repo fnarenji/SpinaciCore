7511fcdaacbe17f09b5b3fa149a98c0c270defc8
sknz <florandara@gmail.com>
2017-03-24 13:43:21 +0100

    [realm] rebase pong on DummyCharEnum

--- wow/src/main/scala/ensiwow/realm/handlers/PingHandler.scala
+++ wow/src/main/scala/ensiwow/realm/handlers/PingHandler.scala
@@ -2,14 +2,14 @@ package ensiwow.realm.handlers
 
 import ensiwow.realm.protocol.payloads.{ClientPing, ServerPong}
 import ensiwow.realm.protocol.{PayloadHandler, PayloadHandlerFactory}
-import ensiwow.realm.session.EventTerminateWithPayload
+import ensiwow.realm.session.NetworkWorker
 
 /**
   * Handles ping packets
   */
 class PingHandler extends PayloadHandler[ClientPing] {
   override def process(payload: ClientPing): Unit = {
-    sender ! EventTerminateWithPayload(ServerPong(payload.ping))
+    sender ! NetworkWorker.EventOutgoing(ServerPong(payload.ping))
   }
 }
 
