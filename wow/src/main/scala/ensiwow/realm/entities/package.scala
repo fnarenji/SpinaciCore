da6df5fa3639496af25e165d3b7ee25fdf86a8ed
sknz <florandara@gmail.com>
2017-04-29 01:36:05 +0200

    [api] print found api modules on startup

---
+++ wow/src/main/scala/ensiwow/realm/entities/package.scala
@@ -0,0 +1,47 @@
+package ensiwow.realm
+
+import ensiwow.realm.shared.{EnumCodecProvider, NumericCodecTag}
+import scodec.codecs._
+
+/**
+  * Created by sknz on 4/28/17.
+  */
+package object entities {
+  object Genders extends Enumeration with EnumCodecProvider[Int] {
+    override protected val valueCodecTag: NumericCodecTag[Int] = uint8L
+
+    val Male = Value(0)
+    val Female = Value(1)
+    val None = Value(2)
+  }
+
+  object Races extends Enumeration with EnumCodecProvider[Int] {
+    override protected val valueCodecTag: NumericCodecTag[Int] = uint8L
+
+    val Human = Value(1)
+    val Orc = Value(2)
+    val Dwarf = Value(3)
+    val Nightelf = Value(4)
+    val UndeadPlayer = Value(5)
+    val Tauren = Value(6)
+    val Gnome = Value(7)
+    val Troll = Value(8)
+    val Bloodelf = Value(10)
+    val Draenei = Value(11)
+  }
+
+  object Classes extends Enumeration with EnumCodecProvider[Int] {
+    override protected val valueCodecTag: NumericCodecTag[Int] = uint8L
+
+    val Warrior = Value(1)
+    val Paladin = Value(2)
+    val Hunter = Value(3)
+    val Rogue = Value(4)
+    val Priest = Value(5)
+    val DeathKnight = Value(6)
+    val Shaman = Value(7)
+    val Mage = Value(8)
+    val Warlock = Value(9)
+    val Druid = Value(11)
+  }
+}
---
+++ wow/src/main/scala/ensiwow/realm/protocol/PacketHandlerHelper.scala
@@ -0,0 +1,52 @@
+package ensiwow.realm.protocol
+
+import akka.actor.{Actor, ActorLogging}
+import ensiwow.utils.Reflection
+
+import scala.language.postfixOps
+
+object PacketHandlerHelper {
+  /**
+    * List of packet handlers companions
+    */
+  private val packetHandlerFactories = Reflection.objectsOf[PacketHandlerFactory[_ <: PacketHandler]]
+
+  /**
+    * List of opcodes for which a handler exists
+    */
+  private val handledOpCodes: Map[OpCodes.Value, PacketHandlerFactory[_ <: PacketHandler]] = packetHandlerFactories.flatMap(
+    factory => {
+      val builder = Map.newBuilder[OpCodes.Value, PacketHandlerFactory[_ <: PacketHandler]]
+      for (elem <- factory.opCodes) {
+        builder += elem -> factory
+      }
+      builder.result()
+    }) toMap
+
+  /**
+    * Handler actor's preferred name
+    *
+    * @param opCode opcode
+    * @return preferred name
+    */
+  def PreferredName(opCode: OpCodes.Value): String = handledOpCodes(opCode).preferredName
+
+  /**
+    * Spawn all actor for handlers
+    */
+  def spawnActors(self: Actor with ActorLogging): Unit = {
+    for (factory <- packetHandlerFactories) {
+      self.context.actorOf(factory.props, factory.preferredName)
+    }
+    self.log.debug(s"Spawned actors for: {}", handledOpCodes.keys.map(_.toString).reduceLeft[String](_ + ", " + _))
+  }
+
+  /**
+    * Indicates if opcode is handled
+    *
+    * @param opCode opcode
+    * @return true if handled, false otherwise
+    */
+  def isHandled(opCode: OpCodes.Value): Boolean = handledOpCodes.contains(opCode)
+}
+
